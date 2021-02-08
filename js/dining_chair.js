1d85ec4c4e4c19ef6cb6300cabf0ba053ba2bffe
Magwest1 <magnus.olstad@gmail.com>
2021-02-08 17:35:29 +0100

    Ugrades to the layout

--- js/dining_chair.js
+++ js/dining_chair.js
@@ -1,7 +1,7 @@
 import "./three.js";
 
-export function normal_chair(width, length, height) {
-  const material = new THREE.MeshBasicMaterial({ color: 0x999999 });
+export function dining_chair(width, length, height) {
+  const material = new THREE.MeshBasicMaterial({ color: 0x008cff });
   const legGeo = new THREE.BoxGeometry(length * 0.1, height, width * 0.1);
   const seatGeo = new THREE.BoxGeometry(length, height * 0.1, width);
   const backGeo = new THREE.BoxGeometry(length, height, width * 0.1);
@@ -31,10 +31,6 @@ export function normal_chair(width, length, height) {
   chair.add(leg4);
   chair.add(seat);
   chair.add(back);
-
-  const geometry2 = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  const material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry2, material2);
 
   return chair;
 }
--- js/preview.js
+++ js/preview.js
@@ -1,36 +1,47 @@
 import "./three.js";
 import {OrbitControls} from "./OrbitControls.js";
-import { normal_chair } from "./normal_chair.js";
+import { dining_chair } from "./dining_chair.js";
 
 let camera, scene, renderer;
 let geometry, material, mesh, controls;
-/*
+
 window.onload = function () {
-  let inputLength = document.getElementById("length");
-  let inputWidth = document.getElementById("width");
-  let inputHeight = document.getElementById("height");
+  let inputLength = document.getElementById("ttlength");
+  let inputWidth = document.getElementById("ttwidth");
+  let inputHeight = document.getElementById("theight");
+  let select = document.getElementById("chairType");
 
   if (inputLength && inputHeight && inputWidth) {
     inputLength.addEventListener("keyup", inputupdate);
     inputWidth.addEventListener("keyup", inputupdate);
     inputHeight.addEventListener("keyup", inputupdate);
+    select.addEventListener("change", changeChair)
   }
 
   function inputupdate() {
-    let length = document.getElementById("length").value / 100;
-    let width = document.getElementById("width").value / 100;
-    let height = document.getElementById("height").value / 100;
+    let length = document.getElementById("ttlength").value / 100;
+    let width = document.getElementById("ttwidth").value / 100;
+    let height = document.getElementById("theight").value / 100;
     if (length != "" && width != "" && height != "") {
       mesh.clear();
-      mesh = normal_chair(length, width, height);
+      mesh = dining_chair(length, width, height);
       scene.add(mesh);
     }
   }
-};
-*/
 
-init(1, 1, 1);
-function init(length, width, height) {
+  function changeChair() {
+    if (select.value == "diningChair") {
+      mesh.clear();
+      mesh = dining_chair(1, 1, 1);
+      scene.add(mesh);
+    }
+  }
+  
+};
+
+
+init();
+function init() {
   camera = new THREE.PerspectiveCamera(
     70,
     2/3,
@@ -39,12 +50,14 @@ function init(length, width, height) {
   );
   camera.position.z = 5;
   scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xffffff );
 
-  mesh = normal_chair(length, width, height);
+  mesh = dining_chair(0, 0, 0);
   scene.add(mesh);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(200,300);
+
   renderer.setAnimationLoop(animation);
   //controls = new DragControls( mesh.children, camera, renderer.domElement );
   controls = new OrbitControls( camera, renderer.domElement );
